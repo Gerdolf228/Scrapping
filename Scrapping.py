8873550d7ce8d79d283261c13ad0f7ebc93b6720
Gerdolf228 <Westigor2@mail.ru>
2023-04-07 15:58:25 +0300

    save scrapping file

--- Scrapping.py
+++ Scrapping.py
@@ -1,1 +1,16 @@
 from bs4 import BeautifulSoup
+import requests
+
+url ="https://health-diet.ru/table_calorie/"
+
+headers = {
+    "accept": "*/*",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+}
+
+req = requests.get(url)
+src = req.text
+#print(src)
+
+with open("index.html", "w") as file:
+    file.write(src)
