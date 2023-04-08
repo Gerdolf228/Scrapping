c2f87a74d6458980081f2f66ee3ccd84611fa831
Gerdolf228 <Westigor2@mail.ru>
2023-04-08 16:11:24 +0300

    Collecting table headers

--- Scrapping.py
+++ Scrapping.py
@@ -4,10 +4,10 @@ import json
 
 #url ="https://health-diet.ru/table_calorie/"
 #
-#headers = {
-#    "accept": "*/*",
-#    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
-#}
+headers = {
+    "accept": "*/*",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+}
 #
 #req = requests.get(url)
 #src = req.text
@@ -33,11 +33,29 @@ import json
 with open("all_categories_dict.json", encoding="utf-8") as file:
     all_categories = json.load(file)
 
+count = 0
 for category_name, category_href in all_categories.items():
 
-    rep = [",", " ", "-", "'"]
-    for item in rep:
-        if item in category_name:
-            category_name = category_name.replace(item, "_")
-    print(category_name)
+    if count ==0:
+        rep = [",", " ", "-", "'"]
+        for item in rep:
+            if item in category_name:
+                category_name = category_name.replace(item, "_")
+
+        req = requests.get(url=category_href, headers=headers)
+        src = req.text
+
+        with open(f"data/{count}_{category_name}.html", "w", encoding="utf-8") as file:
+            file.write(src)
+
+        with open(f"data/{count}_{category_name}.html", encoding="utf-8") as file:
+            src = file.read()
+
+        soup = BeautifulSoup(src, "lxml")
+
+        #Собираем заголовки таблицы
+        table_head = soup.find(class_="mzr-tc-group-table").find("tr").find_all("th")
+        print(table_head)
+
+        count += 1
 
